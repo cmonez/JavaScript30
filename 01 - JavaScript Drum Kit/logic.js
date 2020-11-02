213f25664ce6eb58d3e6ab6ec9faaf151cf80287
Eric Zepeda <zepedaeric@gmail.com>
2020-11-02 13:30:33 -0800

    Progress on logic

--- 01 - JavaScript Drum Kit/logic.js
+++ 01 - JavaScript Drum Kit/logic.js
@@ -3,8 +3,15 @@ window.addEventListener('keydown', (e) => {
   let keyToFind = e.keyCode;
   // querySelector to grab the audio element at the given key
   const audio = document.querySelector(`audio[data-key='${keyToFind}'`);
-  if (!audio) return;
+  // querySelector to grab dom element with class key
+  const key = document.querySelector(`.key[data-key='${keyToFind}'`);
+  // console.log('Heres the key', key);
   // console.log('Here is the audio', audio);
-  // Grabbing whole audio div, and playing it:
-  audio.play();
+  if (!key) return;
+  if (!audio) return;
+  console.log(key.classList); //return class attributes of a specified element
+
+  key.classList.add('playing'); //add playing class from css file
+  audio.currentTime = 0; // audio.currentTime = 0 -> this resets the audio to 0 so that it will restart at zero (aka rewind)
+  audio.play(); // Grabbing whole audio div, and playing it; audio.play -> if clicked while audio playing will NOT rewind
 });
