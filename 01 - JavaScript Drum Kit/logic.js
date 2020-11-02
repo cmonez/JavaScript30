447e555294bd5b85f2f894e61746eda405f5f011
Eric Zepeda <zepedaeric@gmail.com>
2020-11-02 13:14:48 -0800

    Work on logic for playing audio

--- 01 - JavaScript Drum Kit/logic.js
+++ 01 - JavaScript Drum Kit/logic.js
@@ -1,1 +1,10 @@
-console.log('Hey');
+window.addEventListener('keydown', (e) => {
+  // Grab the current key code (from a key down)
+  let keyToFind = e.keyCode;
+  // querySelector to grab the audio element at the given key
+  const audio = document.querySelector(`audio[data-key='${keyToFind}'`);
+  if (!audio) return;
+  // console.log('Here is the audio', audio);
+  // Grabbing whole audio div, and playing it:
+  audio.play();
+});
