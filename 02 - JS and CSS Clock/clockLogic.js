1a5aeedc54d2ac3d32fda6c589afa75b3399eaf4
Eric Zepeda <zepedaeric@gmail.com>
2020-11-03 12:49:17 -0800

    Clock logic continued

--- 02 - JS and CSS Clock/clockLogic.js
+++ 02 - JS and CSS Clock/clockLogic.js
@@ -1,3 +1,6 @@
+// Grab the second hand
+const secondHand = document.querySelector('.second-hand');
+
 function setDate() {
   // Creates a date object
   const now = new Date();
@@ -5,7 +8,10 @@ function setDate() {
   // console.log(seconds);
   // Want to convert the second into degrees for the css transition
   const secondsDegrees = (seconds / 60) * 360;
+  // Rotate second hand by the current second transformed into degrees
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
 }
 
 // setInterval will run a given function for the time that you give it;
+// Since this is updated every second, the secondhand will be current
 setInterval(setDate, 1000);
